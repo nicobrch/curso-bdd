47be9428212fc71221cf844df319939229e9df1a
nicobrch <nicolas.berrios_c@mail.udp.cl>
2022-06-09 15:31:44 -0400

    Tabla periferico y usuario con CARGANDO... y css generico

--- client/src/pages/perifericoList/PerifericosList.jsx
+++ client/src/pages/perifericoList/PerifericosList.jsx
@@ -3,40 +3,38 @@ import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import {useEffect, useState} from "react";
-import { fetchUsers } from "../../Api";
+import {fetchPerifericos} from "../../Api";
 
-export default function UserList() {
+export default function PerifericosList() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetchUsers().then(u => setData(u));
+        fetchPerifericos().then(p => setData(p));
     }, []);
 
     if (data === null) {
-        return <p>Loading profile...</p>;
+        return <p>Loading perifericos...</p>;
     }
 
     const columns = [
         { field: "id", headerName: "ID", width: 90 },
         {
-          field: "username",
-          headerName: "User",
+          field: "marca",
+          headerName: "Marca",
           width: 200,
           renderCell: (params) => {
             return (
-              <div className="itemListItem">
-                <img className="itemListImg" src={params.row.avatar_url} alt="" />
-                {params.row.username}
+              <div className="itemList">
+                <img className="itemListImg" src={params.row.imagen_url} alt="" />
+                {params.row.marca}
               </div>
             );
           },
         },
-        { field: "pp", headerName: "PP", width: 120 },
-        { field: "global_rank", headerName: "Rank G.", width: 150 },
-        { field: "country_rank", headerName: "Rank N.", width: 150 },
-        { field: "playcount", headerName: "Playcount", width: 150 },
-        { field: "country", headerName: "Pais", width: 120 },
-        { field: "updated_at", headerName: "Update", width: 130},
+        { field: "modelo", headerName: "Modelo", width: 240 },
+        { field: "url", headerName: "Link", width: 160 },
+        { field: "tipo_id", headerName: "ID Tipo", width: 130 },
+        { field: "tipo", headerName: "Tipo", width: 150 },
         {
           field: "action",
           headerName: "Action",
